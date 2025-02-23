496caaff9a3dcdac92bab2274f13b2e5bb89b05f
Ria Khakhu <khakhuria99@gmail.com>
2025-02-23 10:39:56 +0200

    problem 21 modified

--- projectEuler/Problem_21_amicable_numbers.java
+++ projectEuler/Problem_21_amicable_numbers.java
@@ -17,12 +17,16 @@ public class Problem_21_amicable_numbers {
     public static int sumOfDivisors(int n)
     {
         int sum = 0;
-        for ( int i = 1; i <= (int)Math.sqrt(n); i++)
+        int sqrt_n = (int)Math.sqrt(n);
+
+        if (n==0 || n == 1){return 0;}
+
+        for ( int i = 1; i <= sqrt_n; i++)
         { 
             if (n%i ==0) 
             {
                 sum += i;
-                if (i != (int)Math.sqrt(n) && i != 1) {sum += (int)n/i;}        
+                if ((sqrt_n*sqrt_n !=n || i != sqrt_n) && (int)n/i!=n) {sum += (int)n/i;}  // if n/i and i are proper factor pairs of n, then add n/i      
             }
         }
         return sum;
@@ -50,6 +54,6 @@ public class Problem_21_amicable_numbers {
 
     public static void main(String args[])
     {
-        printAmicableNumbers(); // every pair is printed twice
+        printAmicableNumbers(); // every pair is printed twice (annoying i know)       
     }
 }
